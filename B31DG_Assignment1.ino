8533323abe94e0e882f098f4e3d4c49c9702b900
Ethan Hunking <ethan.hunking@gmail.com>
2022-02-01 21:28:50 +0000

    Variable Name Change

--- B31DG_Assignment1.ino
+++ B31DG_Assignment1.ino
@@ -1,34 +1,43 @@
 //Constants
-#define button1 22
-#define button2 23
-#define LED1 21
-#define LED2 15
+#define SigA_ 15
+#define SigB_ 21
+#define But1_ 22
+#define But2_ 23
+
+int But1_State = 0;         // variable for reading the pushbutton status
+int But2_State = 0;         // variable for reading the pushbutton status
 
 void setup() {
   // put your setup code here, to run once:
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(button1, INPUT);
-  pinMode(button2, INPUT);
+  // Initialise inputs and outputs to board.
+    pinMode(SigA_, OUTPUT);   //Signal A
+    pinMode(SigB_, OUTPUT);   //Signal B
+    pinMode(But1_, INPUT);    //Button 1 Input
+    pinMode(But2_, INPUT);    //Button 2 Input
+
+  //Set LEDs off
+    digitalWrite(SigA_, LOW);
+    digitalWrite(SigB_, LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  buttonState1 = digitalRead(button1);
-  buttonState2 = digitalRead(button2);
-
-  if(buttonState1 == HIGH){
-    digitalWrite(LED1, HIGH);   // turn the LED 1 on (HIGH is the voltage level)
+  
+  // read the state of the pushbutton value:
+    But1_State = digitalRead(But1_);
+    But2_State = digitalRead(But2_);
+         
+  if(But1_State == HIGH){
+    digitalWrite(SigA_, HIGH);   // turn the LED 1 on (HIGH is the voltage level)
   }
   else{
-    digitalWrite(LED1, LOW);    // turn the LED off by making the voltage LOW
+    digitalWrite(SigA_, LOW);    // turn the LED off by making the voltage LOW
   }
 
-  if(buttonState2 == HIGH){
-    digitalWrite(LED2, HIGH);   // turn the LED 2 on (HIGH is the voltage level)
+  if(But2_State == HIGH){
+    digitalWrite(SigB_, HIGH);   // turn the LED 2 on (HIGH is the voltage level)
   }
   else{
-     digitalWrite(LED2, LOW);    // turn the LED off by making the voltage LOW
+     digitalWrite(SigB_, LOW);    // turn the LED off by making the voltage LOW
   } 
 }
