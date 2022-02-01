0bb03164030a7b4cfa8b8bb59b48f4057d6b9386
Ethan Hunking <ethan.hunking@gmail.com>
2022-02-01 21:19:45 +0000

    Button Test

--- B31DG_Assignment1.ino
+++ B31DG_Assignment1.ino
@@ -1,16 +1,34 @@
+//Constants
+#define button1 22
+#define button2 23
+#define LED1 21
+#define LED2 15
+
 void setup() {
   // put your setup code here, to run once:
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(21, OUTPUT);
-  pinMode(22, OUTPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(button1, INPUT);
+  pinMode(button2, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(21, HIGH);   // turn the LED 1 on (HIGH is the voltage level)
-   digitalWrite(15, HIGH);   // turn the LED 2 on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(21, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(15, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  buttonState1 = digitalRead(button1);
+  buttonState2 = digitalRead(button2);
+
+  if(buttonState1 == HIGH){
+    digitalWrite(LED1, HIGH);   // turn the LED 1 on (HIGH is the voltage level)
+  }
+  else{
+    digitalWrite(LED1, LOW);    // turn the LED off by making the voltage LOW
+  }
+
+  if(buttonState2 == HIGH){
+    digitalWrite(LED2, HIGH);   // turn the LED 2 on (HIGH is the voltage level)
+  }
+  else{
+     digitalWrite(LED2, LOW);    // turn the LED off by making the voltage LOW
+  } 
 }
