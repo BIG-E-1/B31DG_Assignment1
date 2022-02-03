cea179d4c9ca454a806902bfc011596243e24f58
Ethan Hunking <ethan.hunking@gmail.com>
2022-02-03 16:37:17 +0000

    if statements

--- B31DG_Assignment1.ino
+++ B31DG_Assignment1.ino
@@ -8,7 +8,7 @@
 #define B_ 600 //0.6
 #define C_ 17
 #define D_ 2000 //2
-#define Fiftyus 250 //0.05??
+#define Fiftyus 50 
 
 int But1_State = 0;         // variable for reading the pushbutton status
 int But2_State = 0;         // variable for reading the pushbutton status
@@ -34,62 +34,51 @@ void loop() {
     But2_State = digitalRead(But2_);
 
     // Enable Stream of Pulses & run as normal
-    while  ((But1_State == LOW) &&(But2_State == LOW)){
-
-      // read the state of the pushbutton value:
-      But1_State = digitalRead(But1_);
-      But2_State = digitalRead(But2_);
+    if  ((But1_State == LOW) &&(But2_State == LOW)){
 
       //Conts
       int A_Count = A_ ;            //Resets A (pulse length) to original value
 
       //Produce B Pulse
       digitalWrite(SigB_, HIGH);    //Sig B high
-      delay(Fiftyus);               //B signal length of 50us
+      delayMicroseconds(Fiftyus);               //B signal length of 50us
       digitalWrite(SigB_, LOW);     //Sig B low
 
       //Loop for A pulse of C length
       for(int C_Loop = 1; C_Loop <= C_; C_Loop++){ 
         digitalWrite(SigA_, HIGH);   // turn the LED on (HIGH is the voltage level)
-        delay(A_Count);              //Signal A new defined length
+        delayMicroseconds(A_Count);              //Signal A new defined length
         A_Count = A_Count + Fiftyus; //Increments length of pulse A by 50us.   
         digitalWrite(SigA_, LOW);    //Signal A low
-        delay(B_);                   //A signal length of B      
+        delayMicroseconds(B_);                   //A signal length of B      
       }
-      delay(D_);                     //After sequence of pulse. Length pause D.
+      delayMicroseconds(D_);                     //After sequence of pulse. Length pause D.
     }
 
     //Disable stream of pulses 
-    while  (But1_State == HIGH ){
-      But1_State = digitalRead(But1_);
-      But2_State = digitalRead(But2_);
+    if  (But1_State == HIGH ){
       digitalWrite(SigA_, LOW); 
       digitalWrite(SigB_, LOW);
     }
 
     //Enable stream of pulses & run modified 
-    while  ((But1_State == LOW) &&(But2_State == HIGH)){
-
-      // read the state of the pushbutton value:
-      But1_State = digitalRead(But1_);
-      But2_State = digitalRead(But2_);
-
+    if  ((But1_State == LOW) &&(But2_State == HIGH)){
       //Conts
-      int A_Count = A_ + (C_ * Fiftyus) ;            //Resets A (pulse length) to original value
+      int A_Count2 = A_ + (C_ * Fiftyus) ;            //Resets A (pulse length) to original value
 
       //Produce B Pulse
       digitalWrite(SigB_, HIGH);    //Sig B high
-      delay(Fiftyus);               //B signal length of 50us
+      delayMicroseconds(Fiftyus);               //B signal length of 50us
       digitalWrite(SigB_, LOW);     //Sig B low
 
       //Loop for A pulse of C length
       for(int C_Loop = 1; C_Loop <= C_; C_Loop++){ 
         digitalWrite(SigA_, HIGH);   // turn the LED on (HIGH is the voltage level)
-        delay(A_Count);              //Signal A new defined length
-        A_Count = A_Count - Fiftyus; //Increments length of pulse A by 50us.   
+        delayMicroseconds(A_Count2);              //Signal A new defined length
+        A_Count2 = A_Count2 - Fiftyus; //Increments length of pulse A by 50us.   
         digitalWrite(SigA_, LOW);    //Signal A low
-        delay(B_);                   //A signal length of B      
+        delayMicroseconds(B_);                   //A signal length of B      
       }
-      delay(D_);                     //After sequence of pulse. Length pause D.
+      delayMicroseconds(D_);                     //After sequence of pulse. Length pause D.
     }             
 }
